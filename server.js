c575715b5106bdbc48d7a7b16d4d107a0b27eba3
Helton Augusto da Silva <65881598+heltontux@users.noreply.github.com>
2026-04-09 14:54:19 -0300

    Habilitar CORS para requisição cross-origin

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ const fs = require("fs");
 
 const app = express();
 
+// Desbloquear Requisição cross-origin (CORS)
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
 // permite receber POST do ESP32
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
